5a09ea55cae4e5844b6fb23efc1fe90ad214b682
nikolaChakarov <chakarovn1975@gmail.com>
2021-03-31 11:22:52 +0300

    solving cross-origin problem
    add cors library

--- server/config/express.js
+++ server/config/express.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 
 const router = require('../router');
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+
 const configExpress = (app) => {
     app.use(express.json({ extended: false }));
+
+    app.use(cors(corsOptions));
 
     app.use(router);
 }
